e67c216efe4e47b294ed343c95399d05c47db0cf
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-16 20:43:40 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // Test Data
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+// Task2- Function Expression
+const calculateDiscount = function (price, discountRate) {
+    let finalPrice = price - (price * discountRate); // Setting formula.
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`); 
+}; // Declaring function to determine final price. 
+// Test Data 
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
